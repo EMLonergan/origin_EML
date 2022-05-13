1db0af5b2a5aa1f2bcd09841b59db55df127cfdf
EMLonergan92 <emlonergan92@gmail.com>
2022-05-13 10:51:55 +0200

    more extensive data

--- zipper_function.py
+++ zipper_function.py
@@ -4,8 +4,8 @@ def zipper(listA, listB):
     return zipped
 
 if __name__ == '__main__':
-    food = ['pizza', 'sushi', 'jam']
-    people = ['Bob', 'Kevin', 'Angela']
+    food = ['pizza', 'sushi', 'jam', 'pho', 'tofu']
+    people = ['Bob', 'Kevin', 'Angela', 'Meredith']
 
     first_item = zipper(food, people)
     for item in result:
